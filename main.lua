935d841c6ca40b599464d58e1585983f7754d0cf
heyctf <senoritoheyhey@gmail.com>
2025-08-30 17:29:21 -0500

    Refrigerador añadido

--- main.lua
+++ main.lua
@@ -23,8 +23,8 @@ SMODS.Joker{
 		--Lo cual es chistoso porque me tardo muchas más horas que pensaba xd
 			'Todas las {C:green,E:1,S:1.1}probabilidades',
 			'empeoran: {C:green}1 en 2{C:inactive} -> {C:green}1 en 4{C:inactive}',
-			'Cada vez que juegues una carta de la {C:attention}suerte{}',
-			'y esta no se active, gana {C:chips}+#2#{} fichas',
+			'Cada que una carta de la {C:attention}suerte{}',
+			'no se active, obtiene {C:chips}+#2#{} fichas',
 			'{C:inactive}(Actual {C:chips}+#1#{} {C:inactive}fichas)'
 		}
 	},
@@ -63,6 +63,130 @@ SMODS.Joker{
 	end,
 	remove_from_deck = function(self, card, from_debuff)
 		G.GAME.probabilities.normal = G.GAME.probabilities.normal * card.ability.extra.plus_odds
+	end,
+	in_pool = function(self)
+        return true
+    end,
+}
+
+SMODS.Joker:take_ownership('joker',
+    {
+	cost = 5,
+	calculate = function(self, card, context)
+
+	end
+    },
+    true
+)
+
+SMODS.Joker{
+	key = 'refrigerador',
+	loc_txt = {
+		name = 'Refrigerador',
+		text = {
+			'Los comodines {C:attention}perecederos',
+			'y que {C:attention}decrecen con su uso',
+			'{C:inactive,s:0.8}(ej. Palomitas de maíz, Helado, etc.)',
+			'duran el {C:attention}doble'
+		}
+	},
+	cost = 5,
+	rarity = 2,
+	blueprint_compat = true,
+	eternal_compat = true,
+	atlas = 'Jokers',
+	pos = {x=1,y=0},
+	config = {extra = {mano_par=false,descartada_par=false}},
+	update = function(self,card,dt)
+		for i = 1, (G.jokers and G.jokers.cards and #G.jokers.cards or 0) do
+			if G.jokers.cards[i].ability.name == 'Ice Cream' then G.jokers.cards[i].ability.extra.chip_mod = 2.5 end
+			if G.jokers.cards[i].ability.name == 'Popcorn' then G.jokers.cards[i].ability.extra = 2 end
+			if G.jokers.cards[i].ability.name == 'Turtle Bean' then G.jokers.cards[i].ability.extra.h_mod = 0.5 end
+		end
+	end,
+	calculate = function(self,card,context)
+		if context.discard and context.cardarea == G.jokers then
+			if context.blueprint then
+				card.ability.extra.descartada_par = not card.ability.extra.descartada_par
+				for k, v in ipairs(G.jokers.cards) do
+					if v.ability.name == 'Ramen' then
+						if card.ability.extra.descartada_par then
+							v.ability.x_mult = v.ability.x_mult + v.ability.extra
+							return {
+								message = '¡Conservado!',
+								colour = G.C.FILTER
+							}
+						end
+					end
+				end
+			else
+				card.ability.extra.descartada_par = not card.ability.extra.descartada_par
+				for k, v in ipairs(G.jokers.cards) do
+					if v.ability.name == 'Ramen' then
+						if card.ability.extra.descartada_par then
+							v.ability.x_mult = v.ability.x_mult + v.ability.extra
+							return {
+								message = '¡Conservado!',
+								colour = G.C.FILTER
+							}
+						end
+					end
+				end			
+			end
+		end
+		if context.after then
+			for k, v in ipairs(G.jokers.cards) do
+				if v.ability.name == 'Seltzer' then
+					v.ability.extra = v.ability.extra + 0.5
+					return {
+						message = '¡Conservado!',
+						colour = G.C.FILTER
+					}
+				end
+			end
+			if context.blueprint then
+				for k, v in ipairs(G.jokers.cards) do
+					if v.ability.name == 'Ice Cream' then
+						v.ability.extra.chips = v.ability.extra.chips + 2.5
+						return {
+							message = '¡Conservado!',
+							colour = G.C.FILTER
+						}
+					end
+				end
+			end
+		end
+		if context.end_of_round and context.blueprint and context.cardarea == G.jokers then
+			local activado = false
+			for k, v in ipairs(G.jokers.cards) do
+				if v.ability.name == 'Popcorn' then
+					v.ability.mult = v.ability.mult + 2
+				end
+				if v.ability.name == 'Turtle Bean' then
+					v.ability.extra.h_size = v.ability.extra.h_size + 0.5
+				end
+				if v.ability.name == 'Popcorn' or v.ability.name == 'Turtle Bean' then
+					activado = true
+				end
+			end
+			if activado then
+				return {
+					message = '¡Conservado!',
+					colour = G.C.FILTER
+				}
+			end
+		end
+    end,
+	add_to_deck = function(self, card, from_debuff)
+		G.GAME.perishable_rounds = 10
+	end,
+	remove_from_deck = function(self, card, from_debuff)
+		G.GAME.perishable_rounds = 5
+		for k, v in ipairs(G.jokers.cards) do
+			if v.ability.name == 'Popcorn' then v.ability.extra = 4 end
+			if v.ability.name == 'Ice Cream' then v.ability.extra.chip_mod = 5 end
+			if v.ability.name == 'Turtle Bean' then v.ability.extra.h_mod = 1 end
+		end
 	end,
 	in_pool = function(self)
         return true
