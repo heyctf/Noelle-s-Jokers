bf142ed83940e468d2fe5b0af737c39fe59552d2
heyctf <senoritoheyhey@gmail.com>
2025-10-06 09:20:30 -0500

    Archivo de localización de es_ES añadido
    Para evitar problemas con personas que usan el idioma Español de España en lugar de Español de México

---
+++ localization/es_ES.lua
@@ -0,0 +1,182 @@
+return {
+    descriptions = {
+        Back={},
+        Blind={},
+        Edition={
+			e_noelle_tostado = {
+				name = "Tostado",
+				text = {
+					"Se reactiva {C:attention}1{} vez",
+				}
+			}
+		},
+        Enhanced={
+			m_noelle_adicional_primitiva = {
+				name = "Carta adicional primitiva",
+				text = {
+					"{C:chips}+#1#{} fichas extra",
+				}
+			},
+			m_noelle_multi_primitiva = {
+				name = "Carta multi primitiva",
+				text = {
+					"{C:mult}+#1#{} multi",
+				}
+			},
+			m_noelle_suerte_primitiva = {
+				name = "Carta de la suerte primitiva",
+				text = {
+					"{C:green}#3# en 5{} probabilidades",
+					"de {C:mult}+#1#{} multi",
+					"{C:green}#3# en 15{} probabilidades",
+					"de ganar {C:money}#2#$",
+				}
+			}
+		},
+        Joker={
+			j_noelle_gato_negro = {
+				name = "Gato negro",
+				text = {
+					"Todas las {C:green,E:1,S:1.1}probabilidades",
+					"empeoran: {C:green}1 en 2{C:inactive} -> {C:green}1 en 4{C:inactive}",
+					"Cada que una carta {C:attention}de la suerte{}",
+					"no se active, obtiene {C:chips}+#2#{} fichas",
+					"{C:inactive}(Actual {C:chips}+#1#{} {C:inactive}fichas)"
+				}
+			},
+			j_noelle_refrigerador = {
+				name = "Refrigerador",
+				text = {
+					"Los comodines {C:attention}perecederos",
+					"y que {C:attention}decrecen con su uso",
+					"{C:inactive,s:0.8}(ej. Palomitas de maíz, Helado, etc.)",
+					"duran el {C:attention}doble #1#"
+				}
+			},
+			j_noelle_pizza_4_sabores = {
+				name = "Pizza 4 sabores",
+				text = {
+					"Reactiva una vez todas las cartas",
+					"del palo {V:1}#1#{} jugadas",
+					"El palo cambia cada ronda,",
+					"pero puede repetirse"
+				}
+			},
+			j_noelle_aspiradora = {
+				name = "Aspiradora",
+				text = {
+					"Al vencer una ciega jefe quita un",
+					"sticker negativo de un comodín",
+					"y le agrega una edición"
+				}
+			},
+			j_noelle_pintor = {
+				name = "El pintor",
+				text = {
+					"Pinta todas las cartas jugadas",
+					"al palo con más cartas en tu baraja",
+					"si juegas una {C:attention}escalera",
+					"{C:inactive,s:0.8}(Todas las escaleras se",
+					"{C:inactive,s:0.8}considerarán escaleras corridas)",
+					"Palo actual: {V:1}#1#"
+				}
+			},
+			j_noelle_robocop = {
+				name = "El vigilante",
+				text = {
+					"Obtiene {C:chips}+#2#{} fichas cuando",
+					"derrotas una {C:attention}ciega jefe{}",
+					"sin activar su habilidad",
+					"{C:inactive}(Actual {C:chips}+#1#{C:inactive} fichas)",
+				}
+			},
+			j_noelle_gata_parca = {
+				name = "Gata parca",
+				text = {
+					"Evita la muerte {C:attention}#1# veces{} si las",
+					"fichas obtenidas son al menos",
+					"el {C:attention}80%{} de lo requerido y",
+					"obtiene {X:mult,C:white}X#2#{} multi",
+					"Se {S:1.1,C:red,E:2}autodestruye{} cuando",
+					"no le quedan {S:1.1,C:red,E:2}vidas{}",
+					"{C:inactive}(Actual {X:mult,C:white}X#3#{C:inactive} multi)"
+				}
+			},
+			j_noelle_dentista = {
+				name = "Dentista",
+				text = {
+                    "{C:chips}+#1#{} fichas por cada carta",
+                    "sin {C:attention}mejora{}, {C:attention}edición{} ni {C:attention}sello{}",
+					"en tu {C:attention}baraja completa{}",
+					"{C:chips}-#2#{} ficha por cada carta",
+					"con alguna de esas",
+					"{C:inactive}(Se ignoran las cartas de acero){}",
+                    "{C:inactive}(Actual {C:chips}+#3#{C:inactive} fichas)",
+				}
+			},
+			j_noelle_soda_mermelada = {
+				name = "Galleta con mermelada",
+				text = {
+					"Todas las cartas {C:attention}numeradas{} jugadas",
+					"se vuelven cartas {C:attention}multi",
+					"cuando anotan durante {C:attention}#1#{} rondas"
+				}
+			},
+			j_noelle_hucha = {
+				name = "La hucha",
+				text = {
+					"Obtiene el {C:money}${} perdido",
+					"como {C:attention}valor de venta",
+				}
+			}
+		},
+        Other={},
+        Planet={
+			c_noelle_nuevo = {
+				name = "Noelle boba",
+				text = {
+					"Sube de nivel a la mano",
+					"seleccionada si tiene",
+					"{C:attention}3 o más{} palos distintos",
+				}
+			}
+		},
+        Spectral={},
+        Stake={},
+        Tag={},
+        Tarot={},
+        Voucher={},
+    },
+    misc = {
+        achievement_descriptions={},
+        achievement_names={},
+        blind_states={},
+        challenge_names={},
+        collabs={},
+        dictionary={
+			random_suit = "Al azar",
+			ph_mr_bones= "Te salvó un comodín",
+			k_spread_jam = "¡Mermelada!",
+			k_nom_jam = "¡Ñam!",
+		},
+        labels={
+			noelle_tostado = "Tostado",
+		},
+        high_scores={},
+        poker_hand_descriptions={},
+        poker_hands={},
+        quips={},
+        ranks={},
+        suits_plural={
+			noelle_Prehistoria = "La Prehistoria",
+			noelle_Tecnologica = "La Tecnología",
+		},
+        suits_singular={
+			noelle_Prehistoria = "Prehistórico",
+			noelle_Tecnologica = "Tecnológico",
+		},
+        tutorial={},
+        v_dictionary={},
+        v_text={},
+    },
+}
